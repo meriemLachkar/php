d38975f39672fc188c9f03d504171623a5528a2a
meriemLachkar <meriem.lachkar00@gmail.com>
2024-01-07 17:50:08 +0100

    la structure html + la connexion pour le database

--- index.php
+++ index.php
@@ -4,6 +4,15 @@ define('DB_PASS', '');
 define('DB_NAME', 'todolist');
 define('DB_HOST', '127.0.0.1');
 define('DB_PORT', '3306');
+
+$connexion = mysqli_connect(DB_HOST . ':' . DB_PORT, DB_USER, DB_PASS, DB_NAME);
+
+if (!$connexion) {
+    die("La connexion a échoué : " . mysqli_connect_error());
+}
+
+
+mysqli_close($connexion);
 ?>
 <!DOCTYPE html>
 <html>
